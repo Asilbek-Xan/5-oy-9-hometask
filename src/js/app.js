0d290bbed77acf39e77c0f3a9540ba4b562d2cc4
asilbek20000012 <asilbekshakarboev@gmail.com>
2025-07-09 01:13:16 +0500

    yangi commit

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,20 @@ const template = document.querySelector("template");
 const productsList = document.getElementById("products-list");
 
 function updateUi({ products }) {
-    products.forEach(() => {
+    products.forEach((product) => {
+      const {id, title, price, thumbnail, description: _description } = product;
     const clone = template.content.cloneNode(true);
+
+
+    const images = clone.querySelector(".product-image");
+      const cardTitle = clone.querySelector(".card-title");
+    const description = clone.querySelector(".description");
+    const li = clone.querySelector("li");
+
+
+images.src = thumbnail;
+cardTitle.textContent = title;
+// description.textContent =  _description;
 
 
   productsList.appendChild(clone)
